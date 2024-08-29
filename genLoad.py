2bd5cb6daa8d5f5e0980077a3a1c941515114dfa
zhouwei9408@gmail.com <zhouwei9408@gmail.com>
2024-08-29 10:47:18 +0800

    modify

--- genLoad.py
+++ genLoad.py
@@ -10,7 +10,7 @@ from string import Template
 is_debug = True if sys.gettrace() else False
 
 # 时间压缩比
-time_compress = 7
+time_compress = 1
 
 # 构造负载倍数
 load_times = 1
@@ -29,7 +29,7 @@ log = logging.getLogger(__name__)
 csv_file = r'data/test.csv'
 sorted_csv_file = r'data/sorted_test.csv'
 
-cfg_template_path = r'templates/'
+job_template_path = r'templates/'
 
 job_path = r'jobs/'
 deployment_path = r'deployments/'
@@ -41,44 +41,45 @@ def is_replicas(row1, row2):
            and row1['endTime'] == row2['endTime']
 """
 
-def create_yml_files(test_index, replicas, cpu_count, memory_count, is_running, timestamp_duration):
-    config_file_name = '{}config-{}.yml'.format(job_path, str(test_index))
-    test_name = 'test-{}'.format(str(test_index))
-    pod_group_name = 'test-pod-{}'.format(str(test_index))
-    deployment_group_name = 'test-deployment-{}'.format(str(test_index))
-    deployment_file_name = '../{}{}'.format(deployment_path, 'deployment_finished.yml')
-    pod_name = 'test-finished'
 
-    cpu = '{}m'.format(str(cpu_count * 1000))
-    memory = '{}Mi'.format(str(1024 * memory_count))
-    duration = '{}m'.format(timestamp_duration)
+def create_yml_files(test_index, pod_num, duration, cpu_num, memory, gpu_num, is_running):
+    job_file_name = '{}job-{}.yml'.format(job_path, str(test_index))
+    job_name = 'vcjob-{}'.format(str(test_index))
+    min_available = str(pod_num)
+    replicas = str(1)
+    task_name = 'task-{}'.format(str(test_index))
+    pod_name = 'pod-{}'.format(str(test_index))
+    ctr_name = 'ctr-{}'.format(str(test_index))
+    command = '["/bin/bash", "-c", "sleep {}"]'.format(str(duration))
 
-    cfg_template_file = '{}{}'.format(cfg_template_path,
-                                      'running_config_template.yml' if is_running else 'finished_config_template.yml')
-    with open(cfg_template_file, encoding='utf-8') as fp:
+    cpu = '{}m'.format(str(cpu_num * 1000))
+    mem = '{}Mi'.format(str(1024 * memory))
+    gpu = '{}'.format(gpu_num)
+
+    job_template_file = '{}{}'.format(job_template_path,
+                                      'running_job_template.yml' if is_running else 'finished_job_template.yml')
+    with open(job_template_file, encoding='utf-8') as fp:
         read_cfg = fp.read()
-        config_template = Template(read_cfg)
-        cfg_s = config_template.safe_substitute({'testName': test_name,
-                                                 'podGroupName': pod_group_name,
-                                                 'deploymentGroupName': deployment_group_name,
-                                                 'objectTemplatePath': deployment_file_name,
-                                                 'name': pod_name,
-                                                 'replicas': replicas,
-                                                 'cpu': cpu,
-                                                 'memory': memory,
-                                                 'duration': duration})
-    cfg_yaml_data = yaml.safe_load(cfg_s)
-    with open(config_file_name, 'w') as fp:
-        yaml.dump(cfg_yaml_data, fp, sort_keys=False)
+        job_template = Template(read_cfg)
+        job_s = job_template.safe_substitute({'JobName': job_name,
+                                              'MinAvailable': min_available,
+                                              'Replicas': replicas,
+                                              'TaskName': task_name,
+                                              'PodName': pod_name,
+                                              'CtrName': ctr_name,
+                                              'Command': command})
+    job_yaml_data = yaml.safe_load(job_s)
+    with open(job_file_name, 'w') as fp:
+        yaml.dump(job_yaml_data, fp, sort_keys=False)
 
 
 def exec_test(test_index, row_index, time_interval):
-    config_file_name = 'config-{}.yml'.format(str(test_index))
-    print('printing: {} row_index: {} time_interval: {}'.format(config_file_name, row_index, time_interval))
+    job_file_name = 'job-{}.yml'.format(str(test_index))
+    print('printing: {} row_index: {} time_interval: {}'.format(job_file_name, row_index, time_interval))
 
 
 # execute cluster loader task
-def exec_cluster_loader2(test_index):
+def exec_kubectl(test_index):
     job_file_name = '{}job-{}.yml'.format(job_path, str(test_index))
     cmd = 'kubectl apply -f {}'.format(job_file_name)
     os.system(cmd)
@@ -89,6 +90,7 @@ def run():
         df = pd.read_csv(csv_file)
         assert df.shape[0] >= 2
         df = df.sort_values('createDate', ascending=True)
+        df = df.reset_index(drop=True)
         df.to_csv(sorted_csv_file, index=False)
     except Exception as e:
         log.error(e)
@@ -97,8 +99,6 @@ def run():
 
     test_index = 0
     row_index = 0
-    replicas = 1
-    global pre_row
 
     scheduler = sched.scheduler(time.time, time.sleep)
     csv_start_time = df['createDate'][0]
@@ -107,47 +107,23 @@ def run():
     exec_start_time = int(time.time()) + start_interval
 
     for _, row in df.iterrows():
-        if row_index == 0:
-            row_index += 1
-            pre_row = row
-        else:
-            if is_replicas(pre_row, row):
-                replicas += 1
-            else:
-                time_interval = 0 if pre_row['startTime'] < csv_start_time \
-                    else int((pre_row['startTime'] - csv_start_time) / time_compress)
-                is_running = True if pre_row['state'] == 'running' else False
-                duration = 0 if is_running else \
-                    max(1, int((pre_row['endTime'] - max(pre_row['startTime'], csv_start_time)) / (60 * time_compress)))
-                for i in range(load_times):
-                    create_yml_files(test_index,  replicas, pre_row['cpuCount'],
-                                     pre_row['memoryCount'], is_running, duration)
-                    exec_func = exec_test if is_debug else exec_cluster_loader2
-                    exec_func_params = (test_index, row_index, time_interval, ) if is_debug else (test_index, )
-                    if time_interval >= time_interval_threshold:
-                        scheduler.enter(exec_start_time + time_interval + i*load_latency - int(time.time()), 0,
-                                        exec_func, exec_func_params)
-                    test_index += 1
-                replicas = 1
-            row_index += 1
-            pre_row = row
-        if row_index == df.shape[0]:
-            time_interval = 0 if pre_row['startTime'] < csv_start_time \
-                else int((pre_row['startTime'] - csv_start_time) / time_compress)
-            is_running = True if pre_row['state'] == 'running' else False
-            duration = 0 if is_running else \
-                max(1, int((pre_row['endTime'] - max(pre_row['startTime'], csv_start_time)) / (60 * time_compress)))
-            for i in range(load_times):
-                create_yml_files(test_index, replicas, pre_row['cpuCount'],
-                                 pre_row['memoryCount'], is_running, duration)
-                exec_func = exec_test if is_debug else exec_cluster_loader2
-                exec_func_params = (test_index, row_index, time_interval,) if is_debug else (test_index,)
-                if time_interval >= time_interval_threshold:
-                    scheduler.enter(exec_start_time + time_interval + i*load_latency - int(time.time()), 0,
-                                    exec_func, exec_func_params)
-                test_index += 1
-            replicas = 1
+        pod_num = row['worker_num']
+        duration = int((row['endTime'] - row['startTime']) / time_compress)
+        cpu_num = row['cpu_num']
+        memory = row['mem(GB)']
+        gpu_num = row['gpu_num']
+        is_running = (row['status'] == 'running')
 
+        time_interval = int((row['createDate'] - csv_start_time) / time_compress)
+        for i in range(load_times):
+            create_yml_files(test_index, pod_num, duration, cpu_num, memory, gpu_num, is_running)
+            exec_func = exec_test if is_debug else exec_kubectl
+            exec_func_params = (test_index, row_index, time_interval,) if is_debug else (test_index,)
+            if time_interval >= time_interval_threshold:
+                scheduler.enter(exec_start_time + time_interval + i * load_latency - int(time.time()), 0,
+                                exec_func, exec_func_params)
+            test_index += 1
+        row_index += 1
     scheduler.run()
 
 
